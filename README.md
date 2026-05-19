5520dead10ba777975231afdb6f3068532349ccc
Scott Henderson <scott@henderson.engineering>
2026-05-18 19:20:08 -0500

    More changes

--- README.md
+++ README.md
@@ -2,9 +2,36 @@
 
 This is a VERY simple [AppDaemon](https://www.appdaemon.org/) application for the purposes of working with the Enphase Envoy, Ecobee thermostats, and a Wallbox charger. It will **not** be generic, but it may offer ideas.
 
+[Blog post](https://scott.henderson.engineering/2026/05/18/home-power-2.html) can be found here.
+
 ## Theory of the control
 
-TBD
+This is all to maximize my consumption of electricity during daytime hours
+
+This is some kind of Python pseudo code:
+
+```py3
+if not batteryOptimizerEnabled:
+  #don't do stuff if human says no
+  return
+if not gridIsConnected:
+  #should not do anything if I can't depend on the grid at night, but you never know?
+  return
+if hour < 5 or hour > 21:
+  # undo any stateful actions during free use period
+  enableCarCharger()
+  return
+if not acModeIsCooling:
+  #don't need to do anything if the A/C isn't cooling
+  return
+if batteryPercent > 85 and productionInkW > 1.0:
+  #cool this house
+  setACsToOvercool()
+  enableCarCharger()
+else:
+  clearHolds()
+  disableCarCharger()
+```
 
 ## Setup
 
@@ -30,7 +57,7 @@ On the server running Home Assistant, I added another service:
 
 I would use the Docker secret for the token, but it doesn't seem happy.
 
-After that, I run it behind Traefik. I was really struggling with that dash_url because the websocket kept dieing all the time. After testing it directly (direct to an IP address), I determined that the aui (Admin user interface) is borked probably for some easy reason.
+After that, I run it behind Traefik. I was really struggling with that dash_url because the websocket kept dying all the time. After testing it directly (direct to an IP address), I determined that the aui (Admin user interface) seems borked. Considering it must have worked, I bet it's a trivial issue that just requires running it locally.
 
 Therefore, todo items for appdaemon when I have a tiny amount more time:
 
--- batteryoptimizer.py
+++ batteryoptimizer.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from appdaemon import adbase as ad      # Minimalist app base
+from appdaemon.entity import Entity
 from appdaemon.adapi import ADAPI       # Basic API
 from appdaemon.plugins.hass import Hass # Home Assistant-specific API
 
@@ -11,47 +14,75 @@ class BatteryOptimizer(ad.ADBase):
 
     downstairsACEntity = "climate.downstairs"
     upstairsACEntity = "climate.upstairs"
+    downstairsClearHold = "button.downstairs_clear_hold"
+    upstairsClearHold = "button.upstairs_clear_hold"
 
     # acEntityMode = "hvac_mode"
     acTargetTemperature = "temperature"
 
+    hasOptimizedOnce = False
     adapi: ADAPI
     # ha: Hass
     def initialize(self):
         self.adapi = self.get_ad_api()
         # self.ha = self.get_plugin_api("HASS")
-        result = self.adapi.run_every(self.run_every_cycle, "now", 30)
+        result = self.adapi.run_every(self.run_every_cycle, "now", 900)
         self.adapi.log("Battery Optimizer initialized!")
         
     def run_every_cycle(self, **kwargs):
+        time = datetime.now()
+        self.adapi.log(f"Running battery optimization routine at {time}...")
         overallDisabled = self.adapi.get_state(self.overallOnOff) == "on"
         if not overallDisabled:
             self.adapi.log("Battery optimization routine is disabled, skipping...")
             return
         gridOnline = self.adapi.get_state(self.gridStatus) == "on"
+        entityDownstairsClearHold = self.adapi.get_entity(self.downstairsClearHold)
+        entityUpstairsClearHold = self.adapi.get_entity(self.upstairsClearHold)
         if not gridOnline:
-            self.adapi.log("Grid is offline, skipping battery optimization routine...")
+            if self.hasOptimizedOnce:
+                self.adapi.log("Grid is offline, clearing holds and skipping battery optimization routine...")
+                self.ClearHold(entityDownstairsClearHold)
+                self.ClearHold(entityUpstairsClearHold)
+                self.hasOptimizedOnce = False
+            else:
+                self.adapi.log("Grid is offline, skipping battery optimization routine...")
             return
-        
+        current_hour = time.hour
+        if current_hour < 5 or current_hour >= 21:
+            # don't have to worry about resetting here because the ecobee will clear hold as part of changing mode
+            self.adapi.log("Outside of optimization hours, skipping...")
+            self.hasOptimizedOnce = False
+            return
         batteryPercent = self.adapi.get_state(self.batteryTotal)
-        self.adapi.log(f"Running battery optimization routine BP: {batteryPercent}...")
+        panelProduction = self.adapi.get_state(self.production)
+        entityDownstairsAc = self.adapi.get_entity(self.downstairsACEntity)
+        entityUpstairsAc = self.adapi.get_entity(self.upstairsACEntity)
+        entityDownstairsClearHold = self.adapi.get_entity(self.downstairsClearHold)
+        entityUpstairsClearHold = self.adapi.get_entity(self.upstairsClearHold)
 
-        # if batteryPercent < 80 :
+        self.OptimizeAC(batteryPercent, panelProduction, entityDownstairsAc, entityDownstairsClearHold)
+        self.OptimizeAC(batteryPercent, panelProduction, entityUpstairsAc, entityUpstairsClearHold)
+        self.hasOptimizedOnce = True
 
 
-        # entityDownstairsAc = self.adapi.get_entity(self.downstairsACEntity)
-        # entityUpstairsAc = self.adapi.get_entity(self.upstairsACEntity)
-        # if entityDownstairsAc.state == "cool":
-
-        # downstairsEntity = self.adapi.get_entity(self.downstairsACEntity)
-        # downstairsEntityAttributes = downstairsEntity.attributes
-        # self.adapi.log(f"Downstairs AC: {batteryPercent}...")
-        # for x in downstairsEntityAttributes:
-        #     self.adapi.log(f"{x} = {downstairsEntityAttributes[x]}")
-        # downstairsState = downstairsEntity.state
-        # self.adapi.log(f"Downstairs AC state: {downstairsState}...")
-        
-
+    def OptimizeAC(self, batteryPercent, panelProduction, acEntity : Entity, clearHoldEntity : Entity):
+        try:
+            if acEntity.state == "cool":
+                if batteryPercent > 85 and panelProduction > 1.0:
+                    self.adapi.log(f"Battery at {batteryPercent}%, production at {panelProduction}kW, overcooling {acEntity.name} AC...")
+                    acEntity.call_service("climate.set_temperature", data={"temperature": 68})
+                else:
+                    self.adapi.log(f"Battery at {batteryPercent}%, production at {panelProduction}kW, clearing hold...")
+                    self.ClearHold(clearHoldEntity)
+        except Exception as e:
+            self.adapi.log(f"Error optimizing AC {acEntity.name}: {e}")
+    def ClearHold(self, clearHoldEntity : Entity):
+        try:
+            self.adapi.log(f"Clearing hold for {clearHoldEntity.name}...")
+            clearHoldEntity.call_service("button.press")
+        except Exception as e:
+            self.adapi.log(f"Error clearing hold for {clearHoldEntity.name}: {e}")
 
 def main():
     print("Hello from battery-optimizer!")
